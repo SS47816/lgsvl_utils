2bd10fadcb8172f33dd5632d3cb2cd3ca7380b2f
SS47816 <ss1057532022@gmail.com>
2021-12-07 02:41:34 +0800

    feat: add frame transform for autoware objects

--- src/gt_viewer_node.cpp
+++ src/gt_viewer_node.cpp
@@ -1,4 +1,9 @@
-# pragma once
+/* gt_viewer_node.cpp
+
+  Copyright (C) 2021 SS47816 & Advanced Robotics Center, National University of Singapore
+
+  LGSVL Simulator 3D Objects Ground Truth Viewer ROS Node
+*/
 
 #include <iostream>
 #include <string>
@@ -7,7 +12,7 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 
-#include <tf/tf.h>
+// #include <tf/tf.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
@@ -31,47 +36,87 @@ class GTViwerNode
   ros::NodeHandle nh;
   ros::Subscriber lgsvl_gt3d_sub;
   ros::Publisher jsk_bboxes_pub;
-  ros::Publisher autoware_bboxes_pub;
+  ros::Publisher autoware_objects_pub;
 
-  void detections3DCallback(const lgsvl_msgs::Detection3DArray& lgsvl_bboxes);
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf2_listener;
+
+  std::string bbox_target_frame_;
+  
+  void detections3DCallback(const lgsvl_msgs::Detection3DArray& lgsvl_detections3d);
+  autoware_msgs::DetectedObject transformAutowareObject(const lgsvl_msgs::Detection3D& lgsvl_detection3d, geometry_msgs::TransformStamped transform_stamped);
 };
 
-GTViwerNode::GTViwerNode()
+GTViwerNode::GTViwerNode() : tf2_listener(tf_buffer)
 {
   ros::NodeHandle private_nh("~");
   
   std::string lgsvl_gt3d_topic;
   std::string jsk_bboxes_topic;
-  std::string autoware_bboxes_topic;
+  std::string autoware_objects_topic;
+  
   
   ROS_ASSERT(private_nh.getParam("lgsvl_gt3d_topic", lgsvl_gt3d_topic));
   ROS_ASSERT(private_nh.getParam("jsk_bboxes_topic", jsk_bboxes_topic));
-  ROS_ASSERT(private_nh.getParam("autoware_bboxes_topic", autoware_bboxes_topic));
+  ROS_ASSERT(private_nh.getParam("autoware_objects_topic", autoware_objects_topic));
+  ROS_ASSERT(private_nh.getParam("bbox_target_frame", bbox_target_frame_));
 
   lgsvl_gt3d_sub = nh.subscribe(lgsvl_gt3d_topic, 1, &GTViwerNode::detections3DCallback, this);
   jsk_bboxes_pub = nh.advertise<jsk_recognition_msgs::BoundingBoxArray>(jsk_bboxes_topic, 1);
-  autoware_bboxes_pub = nh.advertise<autoware_msgs::DetectedObjectArray>(autoware_bboxes_topic, 1);
+  autoware_objects_pub = nh.advertise<autoware_msgs::DetectedObjectArray>(autoware_objects_topic, 1);
 }
 
-void GTViwerNode::detections3DCallback(const lgsvl_msgs::Detection3DArray& lgsvl_bboxes)
+autoware_msgs::DetectedObject GTViwerNode::transformAutowareObject(const lgsvl_msgs::Detection3D& lgsvl_detection3d, geometry_msgs::TransformStamped transform_stamped)
+{
+  autoware_msgs::DetectedObject autoware_object;
+  // autoware_object.header = lgsvl_detection3d.header;
+  autoware_object.header.frame_id = bbox_target_frame_;
+  geometry_msgs::Pose pose_transformed;
+  tf2::doTransform(lgsvl_detection3d.bbox.position, pose_transformed, transform_stamped);
+  autoware_object.id = lgsvl_detection3d.id;
+  autoware_object.label = lgsvl_detection3d.label;
+  autoware_object.score = lgsvl_detection3d.score;
+  autoware_object.pose = pose_transformed;
+  autoware_object.dimensions = lgsvl_detection3d.bbox.size;
+  autoware_object.velocity = lgsvl_detection3d.velocity;
+
+  return std::move(autoware_object);
+}
+
+void GTViwerNode::detections3DCallback(const lgsvl_msgs::Detection3DArray& lgsvl_detections3d)
 {
   jsk_recognition_msgs::BoundingBoxArray jsk_bboxes;
-  autoware_msgs::DetectedObjectArray autoware_bboxes;
-  jsk_bboxes.header = lgsvl_bboxes.header;
-  autoware_bboxes.header = lgsvl_bboxes.header;
+  // jsk_bboxes.header = lgsvl_detections3d.header;
+  jsk_bboxes.header.frame_id = bbox_target_frame_;
 
-  for (auto const& lgsvl_bbox : lgsvl_bboxes.detections)
+  autoware_msgs::DetectedObjectArray autoware_objects;
+  // autoware_objects.header = lgsvl_detections3d.header;
+  autoware_objects.header.frame_id = bbox_target_frame_;
+  
+  geometry_msgs::TransformStamped transform_stamped;
+  try
+  {
+    transform_stamped = tf_buffer.lookupTransform(bbox_target_frame_, lgsvl_detections3d.header.frame_id, ros::Time(0));
+  }
+  catch (tf2::TransformException& ex)
+  {
+    ROS_WARN("%s", ex.what());
+    return;
+  }
+
+  for (auto const& lgsvl_detection3d : lgsvl_detections3d.detections)
   {
     jsk_recognition_msgs::BoundingBox jsk_bbox;
-    jsk_bbox.header = lgsvl_bbox.header;
-    jsk_bbox.pose = lgsvl_bbox.bbox.position;
-    jsk_bbox.dimensions = lgsvl_bbox.bbox.size;
-    jsk_bbox.value = lgsvl_bbox.score;
-    if (lgsvl_bbox.label == "Pedestrian")
+    // jsk_bbox.header = lgsvl_detection3d.header;
+    jsk_bbox.header.frame_id = bbox_target_frame_;
+    jsk_bbox.pose = lgsvl_detection3d.bbox.position;
+    jsk_bbox.dimensions = lgsvl_detection3d.bbox.size;
+    jsk_bbox.value = lgsvl_detection3d.score;
+    if (lgsvl_detection3d.label == "Pedestrian")
     {
       jsk_bbox.label = 2;
     }
-    else if (lgsvl_bbox.label == "Bicyclist")
+    else if (lgsvl_detection3d.label == "Bicyclist")
     {
       jsk_bbox.label = 1;
     }
@@ -82,20 +127,11 @@ void GTViwerNode::detections3DCallback(const lgsvl_msgs::Detection3DArray& lgsvl
 
     jsk_bboxes.boxes.emplace_back(jsk_bbox);
 
-    autoware_msgs::DetectedObject autoware_bbox;
-    autoware_bbox.header = lgsvl_bbox.header;
-    autoware_bbox.id = lgsvl_bbox.id;
-    autoware_bbox.label = lgsvl_bbox.label;
-    autoware_bbox.score = lgsvl_bbox.score;
-    autoware_bbox.pose = lgsvl_bbox.bbox.position;
-    autoware_bbox.dimensions = lgsvl_bbox.bbox.size;
-    autoware_bbox.velocity = lgsvl_bbox.velocity;
-    
-    autoware_bboxes.objects.emplace_back(autoware_bbox);
+    autoware_objects.objects.emplace_back(transformAutowareObject(lgsvl_detection3d, transform_stamped));
   }
 
   jsk_bboxes_pub.publish(jsk_bboxes);
-  autoware_bboxes_pub.publish(autoware_bboxes);
+  autoware_objects_pub.publish(autoware_objects);
 }
 
 } // namespace lgsvl_utils
