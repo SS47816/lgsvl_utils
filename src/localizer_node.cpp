dd5823375c5f3310c0fe43f1d48c1f0969c77972
SS47816 <ss1057532022@gmail.com>
2021-12-08 04:40:39 +0800

    feat: add ll2 map republish

--- src/localizer_node.cpp
+++ src/localizer_node.cpp
@@ -16,7 +16,7 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <nav_msgs/Odometry.h>
-
+#include <visualization_msgs/MarkerArray.h>
 
 namespace lgsvl_utils 
 {
@@ -29,13 +29,17 @@ class LocalizerNode
 
  private:
   ros::NodeHandle nh;
-  ros::Subscriber lgsvl_odom_sub;
+  ros::Subscriber sub_lgsvl_odom;
+  ros::Subscriber sub_ll2_map_viz;
+  ros::Publisher pub_ll2_map_viz;
   tf2_ros::TransformBroadcaster tf2_broadcaster;
 
+  std::string world_frame_;
   std::string map_frame_;
   std::string baselink_frame_;
   
-  void odomCallback(const nav_msgs::Odometry& lgsvl_odom_msg);
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& lgsvl_odom_msg);
+  void ll2MapCallback(const visualization_msgs::MarkerArray::ConstPtr& ll2_map_viz_msg);
 };
 
 LocalizerNode::LocalizerNode()
@@ -43,27 +47,47 @@ LocalizerNode::LocalizerNode()
   ros::NodeHandle private_nh("~");
   
   std::string lgsvl_odom_topic;
+  std::string input_ll2_map_viz_topic;
+  std::string output_ll2_map_viz_topic;
   
   ROS_ASSERT(private_nh.getParam("lgsvl_odom_topic", lgsvl_odom_topic));
+  ROS_ASSERT(private_nh.getParam("input_ll2_map_viz_topic", input_ll2_map_viz_topic));
+  ROS_ASSERT(private_nh.getParam("output_ll2_map_viz_topic", output_ll2_map_viz_topic));
+  ROS_ASSERT(private_nh.getParam("world_frame", world_frame_));
   ROS_ASSERT(private_nh.getParam("map_frame", map_frame_));
   ROS_ASSERT(private_nh.getParam("baselink_frame", baselink_frame_));
 
-  lgsvl_odom_sub = nh.subscribe(lgsvl_odom_topic, 1, &LocalizerNode::odomCallback, this);
+  sub_lgsvl_odom = nh.subscribe(lgsvl_odom_topic, 1, &LocalizerNode::odomCallback, this);
+  sub_ll2_map_viz = nh.subscribe(input_ll2_map_viz_topic, 1, &LocalizerNode::ll2MapCallback, this);
+  pub_ll2_map_viz = nh.advertise<visualization_msgs::MarkerArray>(output_ll2_map_viz_topic, 1);
 }
 
-void LocalizerNode::odomCallback(const nav_msgs::Odometry& lgsvl_odom_msg)
+void LocalizerNode::odomCallback(const nav_msgs::Odometry::ConstPtr& lgsvl_odom_msg)
 {
-  
   geometry_msgs::TransformStamped transformStamped;
   transformStamped.header.stamp = ros::Time::now();
   transformStamped.header.frame_id = map_frame_;
   transformStamped.child_frame_id = baselink_frame_;
-  transformStamped.transform.translation.x = lgsvl_odom_msg.pose.pose.position.x;
-  transformStamped.transform.translation.y = lgsvl_odom_msg.pose.pose.position.y;
-  transformStamped.transform.translation.z = lgsvl_odom_msg.pose.pose.position.z;
-  transformStamped.transform.rotation = lgsvl_odom_msg.pose.pose.orientation;
+  transformStamped.transform.translation.x = lgsvl_odom_msg->pose.pose.position.x;
+  transformStamped.transform.translation.y = lgsvl_odom_msg->pose.pose.position.y;
+  transformStamped.transform.translation.z = lgsvl_odom_msg->pose.pose.position.z;
+  transformStamped.transform.rotation = lgsvl_odom_msg->pose.pose.orientation;
 
-  tf2_broadcaster.sendTransform(transformStamped);
+  tf2_broadcaster.sendTransform(std::move(transformStamped));
+}
+
+void LocalizerNode::ll2MapCallback(const visualization_msgs::MarkerArray::ConstPtr& ll2_map_viz_msg)
+{
+  visualization_msgs::MarkerArray ll2_world_viz_msg;
+  ROS_WARN("############# There are %d markers in total", int(ll2_map_viz_msg->markers.size()));
+  for (auto &marker : ll2_map_viz_msg->markers)
+  {
+    visualization_msgs::Marker new_marker = marker;
+    new_marker.header.frame_id = world_frame_;
+    ll2_world_viz_msg.markers.emplace_back(std::move(new_marker));
+  }
+  pub_ll2_map_viz.publish(ll2_world_viz_msg);
+  ROS_WARN("############# I Published %d markers in total", int(ll2_world_viz_msg.markers.size()));
 }
 
 } // namespace lgsvl_utils
